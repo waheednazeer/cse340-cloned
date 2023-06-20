dbdd2d1b4f4b35f79e68c81cfa3af8fb215dd1aa
waheednazeer <waheed.nazeer@hotmail.com>
2023-06-20 23:52:56 +0500

    ejs working

---
+++ model-view-w03/server.js
@@ -0,0 +1,43 @@
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
+
+//View Engine and Templates
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Routes
+ *************************/
+
+//app.use(static);
+//app.get('/', function(req, res){
+  //res.render("index",{title: "Home"});
+//});
+app.get('/', baseController.buildHome);
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
