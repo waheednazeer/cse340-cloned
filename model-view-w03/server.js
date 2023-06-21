87839f5c115ed95480840ad1d375ea5b87c9515b
waheednazeer <waheed.nazeer@hotmail.com>
2023-06-21 14:46:45 +0500

    ejs porject is working

--- model-view-w03/server.js
+++ model-view-w03/server.js
@@ -11,21 +11,25 @@ const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
+const inventoryRoute= require('./routes/inventoryRoute.js');
 
 //View Engine and Templates
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("view engine", "ejs");
+
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
 
-//app.use(static);
+app.use(static);
 //app.get('/', function(req, res){
   //res.render("index",{title: "Home"});
 //});
 app.get('/', baseController.buildHome);
+// Inventory routes
+app.use("/inv", inventoryRoute);
 
 /* ***********************
  * Local Server Information
