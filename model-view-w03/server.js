499f9321299c1f59600ead8263662ca6d8bf4b0d
waheednazeer <waheed.nazeer@hotmail.com>
2023-06-22 07:56:30 +0500

    partials folder issue resolved

--- model-view-w03/server.js
+++ model-view-w03/server.js
@@ -12,6 +12,7 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute= require('./routes/inventoryRoute.js');
+const utilities= require('./utilities')
 
 //View Engine and Templates
 app.set("view engine", "ejs");
@@ -27,9 +28,32 @@ app.use(static);
 //app.get('/', function(req, res){
   //res.render("index",{title: "Home"});
 //});
-app.get('/', baseController.buildHome);
+
+app.get('/', utilities.Util.handleErrors(baseController.buildHome));
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Ohh, we can not serve you. Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.Util.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+});
+
+
 
 /* ***********************
  * Local Server Information
