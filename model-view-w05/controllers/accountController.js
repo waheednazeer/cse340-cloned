5c59712c5b07fa037e3f93f86994e999d4fc9375
waheednazeer <waheed.nazeer@hotmail.com>
2023-07-10 02:22:27 +0500

    no changes

--- model-view-w05/controllers/accountController.js
+++ model-view-w05/controllers/accountController.js
@@ -87,6 +87,7 @@ async function accountLogin(req, res) {
   let nav = await utilities.Util.getNav()
   const { account_email, account_password } = req.body
   const accountData = await accountModel.getAccountByEmail(account_email)
+  
   if (!accountData) {
    req.flash("notice", "Please check your credentials and try again.")
    res.status(400).render("account/login", {
